04eabe1fe1ae692a52e8d5c81e4a11b3b7551b74
Dan Minshew <ofenixculpa@gmail.com>
2018-04-06 17:07:20 -0500

    update colors, change logo, remove github icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,40 +2,61 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/ch_logo.svg'
+import animated_logo from '../img/ch_logo_animate.svg'
 
-const Navbar = () => (
-  <nav className="navbar is-transparent">
-    <div className="container">
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
-        </Link>
-      </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
-      </div>
-    </div>
-  </nav>
-)
+export default class NavBar extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      mouseOver: false,
+    }
+  }
 
-export default Navbar
+  mouseEnter = () => {
+    this.setState({ mouseOver: true })
+  }
+  mouseLeave = () => {
+    this.setState({ mouseOver: false })
+  }
+
+  render() {
+    return (
+      <nav className="navbar is-transparent">
+        <div className="container">
+          <div className="navbar-brand">
+            <Link to="/" className="navbar-item">
+              <figure
+                className="image"
+                onMouseEnter={this.mouseEnter}
+                onMouseLeave={this.mouseLeave}
+              >
+                {this.state.mouseOver ? (
+                  <img
+                    src={animated_logo}
+                    alt="Charli Hungry"
+                    style={{ width: '40px', maxHeigh: '2.5em' }}
+                  />
+                ) : (
+                  <img
+                    src={logo}
+                    alt="Charli Hungry"
+                    style={{ width: '40px', maxHeigh: '2.5em' }}
+                  />
+                )}
+              </figure>
+            </Link>
+          </div>
+          <div className="navbar-start">
+            <Link className="navbar-item" to="/about">
+              About
+            </Link>
+            <Link className="navbar-item" to="/products">
+              Products
+            </Link>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Hungry Charli - a nutrition blog" />
     <Navbar />
     <div>{children()}</div>
   </div>
